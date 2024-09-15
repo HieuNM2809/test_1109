8d12a66c7279049f16848e8170e9e05f9dd8bc86
Nguyen Minh Hieu <nmhieucoder@gmail.com>
2024-09-15 16:18:02 +0700

    update command

--- Example CHTC/VD4/app/Console/Commands/updateWardTransShipment.command.js
+++ Example CHTC/VD4/app/Console/Commands/updateWardTransShipment.command.js
@@ -1,27 +1,31 @@
 const Queue = require('bee-queue');
 const redisConfig = require('../../../config/redis');
 const transShipmentsQueue = require('../../Queues/transShipments.queue')
+const WardDistance = require('../../Models/wardDistance.model');
 
-// Tạo và thêm công việc vào hàng đợi
-for (let i = 1; i <= 1000; i++) {
-    transShipmentsQueue.createJob({number: i})
-        .save()
+async function addWardDistancesToQueue() {
+    try {
+        const wardDistances = await WardDistance.findAll({ raw: true });
+
+        for (const distance of wardDistances) {
+            transShipmentsQueue.createJob({
+                ward_id: distance.ward_id,
+                pickup_location_id: distance.pickup_location_id,
+                distance: distance.distance,
+                reverse_distance: distance.reverse_distance
+            })
+                .save()
+                .then(job => {
+                    console.log(`Job created with id: ${job.id} for ward ${distance.ward_id} and pickup location ${distance.pickup_location_id}`);
+                })
+                .catch(error => {
+                    console.error('Failed to create job:', error);
+                });
+        }
+
+    } catch (error) {
+        console.error('Error fetching ward distances:', error);
+    }
 }
 
-
-// const queue = require('./queue');
-// const sequelize = require('./database');
-// require('./jobProcessor');
-//
-// // Tạo và thêm công việc vào hàng đợi
-// for (let i = 1; i <= 1000; i++) {
-//     queue.createJob({ number: i })
-//         .save()
-//         .then((job) => {
-//             console.log(`Job created with id: ${job.id}`);
-//         })
-//         .catch((error) => {
-//             console.error('Job creation failed:', error);
-//         });
-// }
-//
+addWardDistancesToQueue();
